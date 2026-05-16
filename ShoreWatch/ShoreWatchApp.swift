9723969d168d5c60c73b1488840fc805ac47770e
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2026-05-15 20:03:33 -0400

    Initial Commit

---
+++ ShoreWatch/ShoreWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoreWatchApp.swift
+//  ShoreWatch
+//
+//  Created by Fahim Uddin on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ShoreWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
